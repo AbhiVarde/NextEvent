cb280aeeba28505e9a4ab8ce2d181f87709055fe
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2023-11-25 16:54:01 +0530

    feat: added appwrite authentication

--- app/appwrite.tsx
+++ app/appwrite.tsx
@@ -4,7 +4,7 @@ export const client = new Client();
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("653e08b66373b5858139"); // Replace with your project ID
+  .setProject("653e08b66373b5858139");
 
 export const account = new Account(client);
 export { ID } from "appwrite";
--- app/page.tsx
+++ app/page.tsx
@@ -2,54 +2,114 @@
 import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { AiFillGithub } from "react-icons/ai";
+import { account, ID } from "./appwrite";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [loggedInUser, setLoggedInUser] = useState<any>(null);
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-  const handleSignIn = () => {
-    // Add your sign-in logic here using the email and password state values
-    console.log("Signing in with:", email, password);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCredentials((prev) => ({ ...prev, [name]: value }));
   };
 
+  const handleSignIn = async (email: any, password: any) => {
+    try {
+      const session = await account.createEmailSession(email, password);
+      setLoggedInUser(await account.get());
+    } catch (error) {
+      toast.error("Error signing in. Please check your credentials.");
+    }
+  };
+
+  const handleSignUp = async () => {
+    if (!credentials.email || !credentials.password || !credentials.name) {
+      toast.error("Please fill in all the required fields.");
+      return;
+    }
+
+    try {
+      await account.create(
+        ID.unique(),
+        credentials.email,
+        credentials.password,
+        credentials.name
+      );
+      handleSignIn(credentials.email, credentials.password);
+    } catch (error) {
+      toast.error("Error signing up. Please try again.");
+    }
+  };
+
+  const logout = async () => {
+    await account.deleteSession("current");
+    setLoggedInUser(null);
+  };
+
+  if (loggedInUser) {
+    return (
+      <div>
+        <p>Logged in as {loggedInUser.name}</p>
+        <button type="button" onClick={logout}>
+          Logout
+        </button>
+      </div>
+    );
+  }
+
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center">
-      <div className="mx-auto w-full sm:max-w-lg">
+    <div className="min-h-screen bg-[rgba(0,0,0,0.1)] flex flex-col justify-center">
+      <ToastContainer />
+      <div className="px-5 rounded-lg sm:px-10 md:px-0 mx-auto w-full sm:max-w-lg bg-white">
         <div className="p-5 first-letter:bg-white shadow-lg w-full rounded-lg divide-y divide-gray-200">
           <div className="text-center mx-auto my-auto flex flex-col mb-4">
-            <h1 className="text-grey-700 text-xl font-bold">
-              👋 Hello! Welcome to ProEvent!{" "}
+            <h1 className="text-grey-700 text-lg sm:text-xl font-bold">
+              👋 Hello! Welcome to ProEvent!
             </h1>
-            <p className="pt-2 text-gray-500">SignIn Using</p>
+            <p className="pt-2 text-sm sm:text-base text-gray-500">
+              SignIn Using
+            </p>
           </div>
           <div className="pt-4 divide-y divide-gray-200">
-            <form onSubmit={handleSignIn}>
-              <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-600">
-                  Email<span className="text-red-500">*</span>
-                </label>
-                <input
-                  type="email"
-                  className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-blue-500"
-                  placeholder="Enter your email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-600">
-                  Password<span className="text-red-500">*</span>
-                </label>
-                <input
-                  type="password"
-                  className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-blue-500"
-                  placeholder="Enter your password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-              </div>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleSignIn(credentials.email, credentials.password);
+              }}
+            >
+              {["name", "email", "password"].map((field) => (
+                <div key={field} className="mb-4">
+                  <label className="block text-sm font-medium text-gray-600">
+                    {field === "email"
+                      ? "Email"
+                      : field === "password"
+                      ? "Password"
+                      : "Name"}
+                    <span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    type={
+                      field === "email"
+                        ? "email"
+                        : field === "password"
+                        ? "password"
+                        : "text"
+                    }
+                    name={field}
+                    className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder={`Enter your ${field}`}
+                    value={credentials[field as keyof typeof credentials]}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              ))}
               <div className="mb-4">
                 <button
                   type="submit"
@@ -60,23 +120,37 @@ const LoginPage = () => {
               </div>
             </form>
             <div className="pt-4 grid grid-cols-1 md:grid-cols-2 gap-2">
+              {[
+                {
+                  icon: <FcGoogle className="text-xl my-auto" />,
+                  text: "Google",
+                },
+                {
+                  icon: <AiFillGithub className="text-xl my-auto text-black" />,
+                  text: "Github",
+                },
+              ].map((provider, index) => (
+                <button
+                  key={index}
+                  type="button"
+                  className="gap-2 transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal flex align-items-center"
+                >
+                  <div className="flex align-items-center gap-1 justify-center mx-auto">
+                    {provider.icon}
+                    <p className="my-auto">{provider.text}</p>
+                  </div>
+                </button>
+              ))}
+            </div>
+          </div>
+          <div className="pt-4 flex justify-center items-center divide-x-2 text-sm gap-2">
+            <p className="text-gray-500">Don't have an account?</p>
+            <div className="divide-x">
               <button
-                type="button"
-                className="gap-2 transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal flex align-items-center"
+                className="text-black hover:underline font-medium px-2"
+                onClick={handleSignUp}
               >
-                <div className="flex align-items-center gap-1 justify-center mx-auto">
-                  <FcGoogle className="text-xl my-auto" />
-                  <p className="my-auto">Google</p>
-                </div>
-              </button>
-              <button
-                type="button"
-                className="gap-2 transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal flex align-items-center"
-              >
-                <div className="flex align-items-center gap-1 justify-center mx-auto">
-                  <AiFillGithub className="text-xl my-auto text-black" />
-                  <p className="my-auto">Github</p>
-                </div>
+                Signup
               </button>
             </div>
           </div>
