28a0f9e73a5dcd32ab48086529e12bad64ff85c4
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2023-11-26 17:42:54 +0530

    feat: add Homepage and design header

--- app/page.tsx
+++ app/page.tsx
@@ -1,163 +1,116 @@
 "use client";
-import { useState } from "react";
-import { FcGoogle } from "react-icons/fc";
-import { AiFillGithub } from "react-icons/ai";
-import { account, ID } from "./appwrite";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-const LoginPage = () => {
-  const [loggedInUser, setLoggedInUser] = useState<any>(null);
-  const [credentials, setCredentials] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
+interface MenuItemProps {
+  label: string;
+  subMenuItems?: { label: string; link: string }[];
+}
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setCredentials((prev) => ({ ...prev, [name]: value }));
+const MenuItem = ({ label, subMenuItems }: MenuItemProps) => {
+  const [isSubMenuVisible, setIsSubMenuVisible] = useState(false);
+  const handleMouseEnter = () => {
+    setIsSubMenuVisible(true);
   };
-
-  const handleSignIn = async (email: any, password: any) => {
-    try {
-      const session = await account.createEmailSession(email, password);
-      setLoggedInUser(await account.get());
-    } catch (error) {
-      toast.error("Error signing in. Please check your credentials.");
-    }
+  const handleMouseLeave = () => {
+    setIsSubMenuVisible(false);
   };
-
-  const handleSignUp = async () => {
-    if (!credentials.email || !credentials.password || !credentials.name) {
-      toast.error("Please fill in all the required fields.");
-      return;
-    }
-
-    try {
-      await account.create(
-        ID.unique(),
-        credentials.email,
-        credentials.password,
-        credentials.name
-      );
-      handleSignIn(credentials.email, credentials.password);
-    } catch (error) {
-      toast.error("Error signing up. Please try again.");
-    }
-  };
-
-  const logout = async () => {
-    await account.deleteSession("current");
-    setLoggedInUser(null);
-  };
-
-  if (loggedInUser) {
-    return (
-      <div>
-        <p>Logged in as {loggedInUser.name}</p>
-        <button type="button" onClick={logout}>
-          Logout
-        </button>
-      </div>
-    );
-  }
 
   return (
-    <div className="min-h-screen bg-[rgba(0,0,0,0.1)] flex flex-col justify-center">
-      <ToastContainer />
-      <div className="px-5 rounded-lg sm:px-10 md:px-0 mx-auto w-full sm:max-w-lg bg-white">
-        <div className="p-5 first-letter:bg-white shadow-lg w-full rounded-lg divide-y divide-gray-200">
-          <div className="text-center mx-auto my-auto flex flex-col mb-4">
-            <h1 className="text-grey-700 text-lg sm:text-xl font-bold">
-              👋 Hello! Welcome to ProEvent!
-            </h1>
-            <p className="pt-2 text-sm sm:text-base text-gray-500">
-              SignIn Using
-            </p>
-          </div>
-          <div className="pt-4 divide-y divide-gray-200">
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleSignIn(credentials.email, credentials.password);
-              }}
-            >
-              {["name", "email", "password"].map((field) => (
-                <div key={field} className="mb-4">
-                  <label className="block text-sm font-medium text-gray-600">
-                    {field === "email"
-                      ? "Email"
-                      : field === "password"
-                      ? "Password"
-                      : "Name"}
-                    <span className="text-red-500">*</span>
-                  </label>
-                  <input
-                    type={
-                      field === "email"
-                        ? "email"
-                        : field === "password"
-                        ? "password"
-                        : "text"
-                    }
-                    name={field}
-                    className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-blue-500"
-                    placeholder={`Enter your ${field}`}
-                    value={credentials[field as keyof typeof credentials]}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-              ))}
-              <div className="mb-4">
-                <button
-                  type="submit"
-                  className="w-full bg-black text-white py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-medium"
-                >
-                  Sign In
-                </button>
+    <div
+      className="relative inline-block "
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {subMenuItems ? (
+        <>
+          <span className="cursor-pointer text-sm text-[#888888] hover:text-[#ededed] active:text-[#ededed]">
+            {label}
+          </span>
+          {isSubMenuVisible && (
+            <div className="origin-top-right absolute  mt-2 w-32 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="py-1">
+                {subMenuItems.map((item) => (
+                  <Link
+                    key={item.label}
+                    href={item.link}
+                    passHref
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    {item.label}
+                  </Link>
+                ))}
               </div>
-            </form>
-            <div className="pt-4 grid grid-cols-1 md:grid-cols-2 gap-2">
-              {[
-                {
-                  icon: <FcGoogle className="text-xl my-auto" />,
-                  text: "Google",
-                },
-                {
-                  icon: <AiFillGithub className="text-xl my-auto text-black" />,
-                  text: "Github",
-                },
-              ].map((provider, index) => (
-                <button
-                  key={index}
-                  type="button"
-                  className="gap-2 transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal flex align-items-center"
-                >
-                  <div className="flex align-items-center gap-1 justify-center mx-auto">
-                    {provider.icon}
-                    <p className="my-auto">{provider.text}</p>
-                  </div>
-                </button>
-              ))}
             </div>
-          </div>
-          <div className="pt-4 flex justify-center items-center divide-x-2 text-sm gap-2">
-            <p className="text-gray-500">Don't have an account?</p>
-            <div className="divide-x">
-              <button
-                className="text-black hover:underline font-medium px-2"
-                onClick={handleSignUp}
-              >
-                Signup
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+          )}
+        </>
+      ) : (
+        <Link
+          href={`/${label.toLowerCase()}`}
+          passHref
+          className="text-sm text-[#888888] hover:text-[#ededed] active:text-[#ededed]"
+        >
+          {label}
+        </Link>
+      )}
     </div>
   );
 };
 
-export default LoginPage;
+const Home = () => {
+  const router = useRouter();
+  const menuItems: MenuItemProps[] = [
+    { label: "Dashboard" },
+    {
+      label: "Events",
+      subMenuItems: [
+        { label: "Upcoming Events", link: "/events/upcoming" },
+        { label: "Past Events", link: "/events/past" },
+        { label: "Create New Event", link: "/events/create" },
+      ],
+    },
+    {
+      label: "Venues",
+      subMenuItems: [
+        { label: "All Venues", link: "/venues/all" },
+        { label: "Add New Venue", link: "/venues/add" },
+      ],
+    },
+    { label: "About" },
+    { label: "Contact" },
+  ];
+
+  return (
+    <div className="min-h-screen font-Inter">
+      <nav className="bg-black border-b border-gray-200">
+        <div className="container mx-auto px-16">
+          <div className="flex justify-between items-center py-5">
+            <div className="flex items-center space-x-6">
+              <button className="text-base sm:text-lg font-medium text-white">
+                NextEvent
+              </button>
+              <div className="space-x-4">
+                {menuItems.map((item) => (
+                  <MenuItem
+                    key={item.label}
+                    label={item.label}
+                    subMenuItems={item.subMenuItems}
+                  />
+                ))}
+              </div>
+            </div>
+            <button
+              className="bg-[#ededed] text-sm py-2 px-3 font-medium p-[0px 12px] rounded-md"
+              onClick={() => router.push("/login")}
+            >
+              LogIn
+            </button>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Home;
